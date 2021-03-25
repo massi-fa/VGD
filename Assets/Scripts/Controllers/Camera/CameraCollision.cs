23520d52909151d0f73eadcfca8e2199b3d8ff93
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2021-03-26 00:29:52 +0100

    fix zoom

--- Assets/Scripts/Controllers/Camera/CameraCollision.cs
+++ Assets/Scripts/Controllers/Camera/CameraCollision.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class CameraCollision : MonoBehaviour
 {
-    public float minDistance = 1.5f;
-    public float maxDistance = 4.0f;
+    public float minDistance = 1.0f;
+    public float maxDistance = 3.0f;
     public float smooth = 10.0f;
     Vector3 dollyDir;
     public Vector3 dollyDirAdjusted;
@@ -23,7 +23,7 @@ public class CameraCollision : MonoBehaviour
         Vector3 desiredCameraPos = transform.parent.TransformPoint (dollyDir*maxDistance);
         RaycastHit hit;
         if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit)){
-            distance = Mathf.Clamp (hit.distance, minDistance, maxDistance);
+            distance = Mathf.Clamp (hit.distance*0.9f, minDistance, maxDistance);
         }else{
             distance = maxDistance;
         }
