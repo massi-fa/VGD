d1679dc4da4855b6bb600dd05faf34a57b4cb03a
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2021-03-30 21:57:17 +0200

    menu and healt bar

---
+++ Assets/Scripts/Button/ExitFromGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+public class ExitFromGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene(0);
+        }
+    }
+}
